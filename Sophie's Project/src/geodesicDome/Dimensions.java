4a924ad9515374e266d08940c77acac64717a0c0
KiranGovindUCL <zcapkgo@ucl.ac.uk>
2015-11-24 12:19:20 +0000

    Sophie's Project: added angle between iso and equil, not quite correct perhaps

--- Sophie's Project/src/geodesicDome/Dimensions.java
+++ Sophie's Project/src/geodesicDome/Dimensions.java
@@ -52,22 +52,77 @@ public class Dimensions {
 
 	public static double insideFiveLengthFlatPentagon() {
 		double d = base / (2 * Math.sin(Math.toRadians(36)));
+		// System.out.println("d = " + d);
 		return d;
 	}
 
-	public static double taperAngleOfBeamsInPentagon () {
+	public static double taperAngleOfCentralBeamsInPentagon() {
+
 		double ang1 = Math.toRadians(72);
-		ThreeVector A = new ThreeVector(-base*Math.sin(ang1),base*Math.cos(ang1),0);
-		
-		double g = (base*Math.sin(Math.toRadians(74))/(2*Math.sin(Math.toRadians(36))));
+		ThreeVector A = new ThreeVector(-base * Math.sin(ang1), base * Math.cos(ang1), 0);
+		// System.out.println("A: " + A);
+		double g = (base * Math.sin(Math.toRadians(54)) / (2 * Math.sin(Math.toRadians(36))));
 		double d = insideFiveLengthFlatPentagon();
-		double e = Math.sqrt(Math.pow(hypotenuse, 2) + Math.pow(d, 2));
-		ThreeVector B = new ThreeVector(0.5*base, g, e);
+		double e = Math.sqrt(Math.pow(hypotenuse, 2) - Math.pow(d, 2));
+		// System.out.println("Hypotenuse: " + hypotenuse);
+		// System.out.println("e: " + e);
+		ThreeVector B = new ThreeVector(0.5 * base, g, e);
+		// System.out.println("B: " + B);
+		ThreeVector C = new ThreeVector(0.5 * base, (g + d), 0);
+		ThreeVector O = new ThreeVector(0, 0, 0);
+
+		// System.out.println("C: " + C);
+		ThreeVector v1 = ThreeVector.subtract(A, O);
+		ThreeVector v2 = ThreeVector.subtract(B, O);
+
+		ThreeVector v3 = ThreeVector.subtract(B, A);
+		ThreeVector v4 = ThreeVector.subtract(C, A);
+
+		ThreeVector v1xv2 = ThreeVector.vectorProduct(v1, v2);
+		ThreeVector v3xv4 = ThreeVector.vectorProduct(v3, v4);
+		// System.out.println("v1xv2: " + v1xv2);
+		// System.out.println("v3xv4: " + v3xv4);
+
+		double angle = 0.5 * Math.toDegrees(ThreeVector.angle(v1xv2, v3xv4));
+
+		System.out.println("Taper Angle Of Beams In Pentagon: " + angle + "\u00b0");
+
+		return angle;
+	}
+
+	public static double taperAngleOfIsoEquil() {
+		double b = base;
+		double q = (b*Math.sin(Math.toRadians(54))/2)+b*Math.sin(Math.toRadians(6));
+		double phi = Math.acos(b*Math.sin(Math.toRadians(54))/(2*q));
+		double gamma = Math.acos(b*b*Math.cos(phi)*Math.sin(Math.toRadians(60))
+				/Math.sin(Math.toRadians(72)));
+		double c = hypotenuse;
+		double d = insideFiveLengthFlatPentagon();
 		
-		ThreeVector C = new ThreeVector(0.5*base, (g + d), 0);
-		ThreeVector O = new ThreeVector(0,0,0);
+		ThreeVector A = new ThreeVector(0,0,0);
+		ThreeVector D = new ThreeVector((-b*Math.sin(Math.toRadians(36))),(b*Math.cos(Math.toRadians(36))),0);
+		ThreeVector C = new ThreeVector(((Math.sin(gamma))*Math.sin(Math.toRadians(72))),
+				(b*Math.sin(Math.toRadians(18))),(b * Math.sin(Math.toRadians(60))*Math.cos(phi)));
+		ThreeVector B = new ThreeVector((-c*Math.sin(gamma-phi)),(-0.5*b),Math.sqrt((c*c)-(d*d)));
 		
-		ThreeVector v1 = ThreeVector.(C,O);
+		ThreeVector v1 = ThreeVector.subtract(B, A);
+		ThreeVector v2 = ThreeVector.subtract(C, A);
+		ThreeVector v3 = ThreeVector.subtract(D, A);
+		
+		ThreeVector v1xv2 = ThreeVector.vectorProduct(v1, v2);
+		ThreeVector v2xv3 = ThreeVector.vectorProduct(v2, v3);
+		
+		double angle = 0.5 * Math.toDegrees(ThreeVector.angle(v1xv2, v2xv3));
+
+		System.out.println("Taper Angle Between Iso and Equil: " + angle + "\u00b0");
+		
+		return angle;
+	}
+
+	public static double taperAngleBetweenIsoAndEquil() {
+
+		double angle = 0;
+		return angle;
 	}
 
 	public static void main(String[] args) {
@@ -75,9 +130,12 @@ public class Dimensions {
 		System.out.println("----------------PROJECT DOMEY----------------");
 		System.out.println("---------------------------------------------");
 		System.out.println();
-		ArrayList<Double> aOI = anglesOfIsosceles(hypotenuse, base);
+		anglesOfIsosceles(hypotenuse, base);
 		System.out.println();
 		diameter(decagonSideLength);
+		System.out.println();
+		taperAngleOfCentralBeamsInPentagon();
+		taperAngleOfIsoEquil();
 	}
 
 }
--- Sophie's Project/src/geodesicDome/ThreeVector.java
+++ Sophie's Project/src/geodesicDome/ThreeVector.java
@@ -1,99 +1,98 @@
 package geodesicDome;
 
 public class ThreeVector {
-		
 
-	 	//Every ThreeVector has its own pr_x, pr_y, pr_z
-	    private double pr_x, pr_y, pr_z;
-	
-	    //Empty constructor
-	    public ThreeVector() {}
-		
-	    //ThreeVector constructor and set the variables to the private
-	    //variables to be used by the class.
-		public ThreeVector(double pu_x, double pu_y, double pu_z) {
-			pr_x = pu_x;
-			pr_y = pu_y;
-			pr_z = pu_z;
-		}
-		
-		//Magnitude of the vector
-		public double magnitude() {
-			return Math.sqrt(pr_x * pr_x + pr_y * pr_y + pr_z * pr_z);
-		}
-		
-		//Create unit vectors, using the magnitude defined for the
-		//variables entered at that instance
-		public ThreeVector unitVector () {
-			double uV_x = pr_x/this.magnitude();
-			double uV_y = pr_y/this.magnitude();
-			double uV_z = pr_z/this.magnitude();
-			
-			return new ThreeVector(uV_x, uV_y, uV_z);
-		}
-		
-		//Creates a string 
-		public String toString () {
-			return "("+pr_x+","+pr_y+","+pr_z+")";
-		}
-		
-		//Create a static method that returns the scalar product
-		//between two vectors.
-		public static double scalarProduct (ThreeVector vecA, 
-				ThreeVector vecB) {
-			return (vecA.pr_x*vecB.pr_x + vecA.pr_y*vecB.pr_y + 
-					vecA.pr_z*vecB.pr_z);
-		}
-		
-		//Create a static method that returns a ThreeVector of the
-		//vector product between two vectors.
-		public static ThreeVector vectorProduct (ThreeVector vecA, 
-				ThreeVector vecB) {
-			double vP_x = (vecA.pr_x * vecB.pr_y) - 
-					(vecA.pr_y * vecB.pr_x);
-			double vP_y = (vecA.pr_y * vecB.pr_z) - 
-					(vecA.pr_z * vecB.pr_y);
-			double vP_z = (vecA.pr_z * vecB.pr_x) - 
-					(vecA.pr_x * vecB.pr_z);						
-			return new ThreeVector (vP_x, vP_y, vP_z);
-		}
-		
-		//Static method to add two vectors and return a ThreeVector
-		public static ThreeVector add (ThreeVector vecA, 
-				ThreeVector vecB) {
-			return new ThreeVector (vecA.pr_x+vecB.pr_x,
-					vecA.pr_y+vecB.pr_y , vecA.pr_z+vecB.pr_z);
-		}
-		//Static method to return the angle between two vectors as a 
-		//double.
-		public static double angle(ThreeVector vecA, ThreeVector vecB) {
-			double sP = ThreeVector.scalarProduct(vecA,vecB);
-			double magA = vecA.magnitude();
-			double magB = vecA.magnitude();
-			return Math.acos(sP/(magA*magB));
-		}
-		//Create a static method that returns the scalar product
-		//between two vectors.
-		public double scalarProduct(ThreeVector vec) {
-			return scalarProduct(this, vec);
-		}
-		
-		//Create a non-static method that returns a ThreeVector of the
-		//vector product between two vectors.
-		public ThreeVector add(ThreeVector vec) {
-			return add(this, vec);
-		}
-		
-		//Non-static method to add two vectors and return a ThreeVector
-		public ThreeVector vectorProduct(ThreeVector vec) {
-			return vectorProduct(this, vec);
-		}
-		
-		//Non-static method to return the angle between two vectors as 
-		//a double.
-		public double angle(ThreeVector vec) {
-			return angle(this, vec);
-		}
-		
-		
+	// Every ThreeVector has its own pr_x, pr_y, pr_z
+	private double pr_x, pr_y, pr_z;
+
+	// Empty constructor
+	public ThreeVector() {
+	}
+
+	// ThreeVector constructor and set the variables to the private
+	// variables to be used by the class.
+	public ThreeVector(double pu_x, double pu_y, double pu_z) {
+		pr_x = pu_x;
+		pr_y = pu_y;
+		pr_z = pu_z;
+	}
+
+	// Magnitude of the vector
+	public double magnitude() {
+		return Math.sqrt(pr_x * pr_x + pr_y * pr_y + pr_z * pr_z);
+	}
+
+	// Create unit vectors, using the magnitude defined for the
+	// variables entered at that instance
+	public ThreeVector unitVector() {
+		double uV_x = pr_x / this.magnitude();
+		double uV_y = pr_y / this.magnitude();
+		double uV_z = pr_z / this.magnitude();
+
+		return new ThreeVector(uV_x, uV_y, uV_z);
+	}
+
+	// Creates a string
+	public String toString() {
+		return "(" + pr_x + "," + pr_y + "," + pr_z + ")";
+	}
+
+	// Create a static method that returns the scalar product
+	// between two vectors.
+	public static double scalarProduct(ThreeVector vecA, ThreeVector vecB) {
+		return (vecA.pr_x * vecB.pr_x + vecA.pr_y * vecB.pr_y + vecA.pr_z * vecB.pr_z);
+	}
+
+	// Create a static method that returns a ThreeVector of the
+	// vector product between two vectors.
+	public static ThreeVector vectorProduct(ThreeVector vecA, ThreeVector vecB) {
+		double vP_x = (vecA.pr_x * vecB.pr_y) - (vecA.pr_y * vecB.pr_x);
+		double vP_y = (vecA.pr_y * vecB.pr_z) - (vecA.pr_z * vecB.pr_y);
+		double vP_z = (vecA.pr_z * vecB.pr_x) - (vecA.pr_x * vecB.pr_z);
+		return new ThreeVector(vP_x, vP_y, vP_z);
+	}
+
+	// Static method to add two vectors and return a ThreeVector
+	public static ThreeVector add(ThreeVector vecA, ThreeVector vecB) {
+		return new ThreeVector(vecA.pr_x + vecB.pr_x, vecA.pr_y + vecB.pr_y, vecA.pr_z + vecB.pr_z);
+	}
+
+	// Static method to subtract two vectors and return a ThreeVector
+	public static ThreeVector subtract(ThreeVector vecA, ThreeVector vecB) {
+		return new ThreeVector(vecA.pr_x - vecB.pr_x, vecA.pr_y - vecB.pr_y, vecA.pr_z - vecB.pr_z);
+	}
+
+	// Static method to return the angle between two vectors as a
+	// double.
+	public static double angle(ThreeVector vecA, ThreeVector vecB) {
+		double sP = ThreeVector.scalarProduct(vecA, vecB);
+		double magsP = Math.abs(sP);
+		double magA = vecA.magnitude();
+		double magB = vecB.magnitude();
+		return Math.acos(magsP / (magA * magB));
+	}
+
+	// Create a static method that returns the scalar product
+	// between two vectors.
+	public double scalarProduct(ThreeVector vec) {
+		return scalarProduct(this, vec);
+	}
+
+	// Create a non-static method that returns a ThreeVector of the
+	// vector product between two vectors.
+	public ThreeVector add(ThreeVector vec) {
+		return add(this, vec);
+	}
+
+	// Non-static method to add two vectors and return a ThreeVector
+	public ThreeVector vectorProduct(ThreeVector vec) {
+		return vectorProduct(this, vec);
+	}
+
+	// Non-static method to return the angle between two vectors as
+	// a double.
+	public double angle(ThreeVector vec) {
+		return angle(this, vec);
+	}
+
 }
